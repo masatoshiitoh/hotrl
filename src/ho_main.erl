bb99c32b6cd6d7dd755f9d860c2805c3688e70fb
Masatoshi Itoh <masatoshi9953@gmail.com>
2016-05-25 19:27:42 +0900

    initial commit

---
+++ src/ho_main.erl
@@ -0,0 +1,46 @@
+-module(ho_main).
+-behaviour(gen_server).
+
+-export([add/2, sub/2]).
+
+-export([start_link/1]).
+-export([terminate/2]).
+-export([init/1]).
+-export([handle_call/3]).
+
+%%
+%% 外部向けAPI
+%%
+
+add(A,B) ->
+	Reply = gen_server:call(?MODULE, {add, A, B}),
+	{ok, V} = Reply,
+	V.
+
+sub(A,B) ->
+	Reply = gen_server:call(?MODULE, {sub, A, B}),
+	{ok, V} = Reply,
+	V.
+
+%%
+%% ビヘイビアのコールバック定義（処理の実体）
+%%
+
+start_link(_Opts) ->
+	Args = [],
+	gen_server:start_link({local, ?MODULE}, ?MODULE, Args, []).
+
+init(_Args) ->
+	NewState = [],
+	{ok, NewState}.
+
+terminate(_Reason, State) ->
+	ok.
+
+handle_call({add, A, B}, From, State) ->
+	{reply, {ok, A+B}, State};
+
+handle_call({sub, A, B}, From, State) ->
+	{reply, {ok, A-B}, State}.
+
+
